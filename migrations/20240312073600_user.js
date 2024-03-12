c9820644108ad1232f1ae3d47bc30936948304c1
harshil-bodara <developer.harshilbodara@gmail.com>
2024-03-12 13:59:42 +0530

    connect a postgressql and create the migration

---
+++ migrations/20240312073600_user.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable("user", (table) => {
+        table.increments("id").primary(); 
+        table.string("firstname").notNullable();
+        table.text("lastname").notNullable(); 
+        table.string("email").notNullable(); 
+        table.timestamps(true, true); 
+      });    
+
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists("user");
+};
